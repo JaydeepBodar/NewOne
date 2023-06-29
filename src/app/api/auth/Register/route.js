354b9cda8fb78bc03b94590d1b1a2346b02865b8
JaydeepBodar <jaydeepbodar732@gmail.com>
2023-06-29 18:30:53 +0530

    auth1

--- src/app/api/auth/Register/route.js
+++ src/app/api/auth/Register/route.js
@@ -5,6 +5,7 @@ import bcrypt from 'bcrypt'
 import React from 'react'
 
 export const POST = async(req,res) => {
+	console.log("rew",req)
  try{
     await db()
     const {name,email,password}=await req.json()
